ba8363cb9f26d899818313f223a231d4e4bb8576
Adi016prasad <130592410+Adi016prasad@users.noreply.github.com>
2025-05-22 14:20:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import classification  
+import classification
+import os
 
 app = Flask(__name__)
 CORS(app)  
@@ -25,4 +26,5 @@ def classify_email():
     return result
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5007)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
